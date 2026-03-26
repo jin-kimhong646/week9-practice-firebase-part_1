f7b5d214b88c198b0ceea596ea0f64ad294ddcc1
jin-kimhong646 <kimhong.chhour@student.cadt.edu.kh>
2026-03-26 19:17:41 +0700

    W9-01 – Fetch and display songs in the library screen

---
+++ lib/data/repositories/songs/song_repository_firebase.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../../../model/songs/song.dart';
+import '../../dtos/song_dto.dart';
+import 'song_repository.dart';
+
+class SongRepositoryFirebase extends SongRepository {
+  final Uri songsUri = Uri.https(
+    'g2-love-ronan-default-rtdb.asia-southeast1.firebasedatabase.app',
+    '/songs.json',
+  );
+
+  @override
+  Future<List<Song>> fetchSongs() async {
+    final http.Response response = await http.get(songsUri);
+
+    if (response.statusCode == 200) {
+      // 1 - Send the retrieved list of songs
+      Map<String, dynamic> songJson = json.decode(response.body);
+      return songJson.entries
+          .map((entry) => SongDto.fromJson(entry.key, entry.value))
+          .toList();
+    } else {
+      // 2- Throw expcetion if any issue
+      throw Exception('Failed to load posts');
+    }
+  }
+
+  @override
+  Future<Song?> fetchSongById(String id) async {}
+}
---
+++ lib/ui/widgets/song/song_tile.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../../model/songs/song.dart';
+
+class SongTile extends StatelessWidget {
+  const SongTile({
+    super.key,
+    required this.song,
+    required this.isPlaying,
+    required this.onTap,
+  });
+
+  final Song song;
+  final bool isPlaying;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: ListTile(
+          onTap: onTap,
+          leading: ClipOval(
+            child: Image.network(
+              '${song.imageUrl}',
+              width: 45,
+              height: 45,
+              fit: BoxFit.cover,
+            ),
+          ),
+          title: Text(song.title),
+          subtitle: Text('${song.duration.inMinutes} mins'),
+          trailing: Text(
+            isPlaying ? "Playing" : "",
+            style: TextStyle(color: Colors.amber),
+          ),
+        ),
+      ),
+    );
+  }
+}
